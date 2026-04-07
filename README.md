fa9f0d4b427160efcb3ae94837963871b6857bb3
Guido Flohr <guido.flohr@cantanea.com>
2026-04-07 10:54:18 +0300

    add missing bits and pieces

---
+++ README.md
@@ -0,0 +1,6 @@
+# Font Embedder
+
+This is a temporary repository for experimenting with
+[@cantoo-scribe/pdf-lib](https://github.com/cantoo-scribe/pdf-lib).
+Its aim is to find out, how to embed the 14 standard PDF fonts into a PDF
+document.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello, world!');
+import * as fs from 'node:fs/promises';
+import * as os from 'node:os';
+import fontkit from '@pdf-lib/fontkit';
+
+let arialPath: string;
+
+switch (os.platform()) {
+	case 'darwin':
+		arialPath = '/System/Library/Fonts/Supplemental/Arial.ttf';
+		break;
+	case 'win32':
+		arialPath = 'C:/Windows/Fonts/arial.ttf';
+		break;
+	default:
+		arialPath = '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf';
+		break;
+}
+
+async function embedFonts() {
+	const fontBytes = await fs.readFile(arialPath);
+	const fontkitFont = fontkit.create(fontBytes);
+	console.log(fontkitFont);
+}
+
+embedFonts();
